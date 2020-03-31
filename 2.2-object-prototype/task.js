df290eced15b0bc78f84504cffe934c8fb98a300
Alexandr Varygin <otz-job@mail.ru>
2020-03-31 19:14:35 +0300

    2.2 object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,46 @@
+"use strict";
+
+
+// task 1
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    return (typeof animal == "undefined") ? null : animal.sound;    
 }
 
+
+// task 2
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    
+    let arrayMarksNumber = [];    
+    let roundedAverageMark = 0;
+    let sum = 0;  
+
+    
+    if (marks.length === 0) {
+        roundedAverageMark = 0;
+    } else {
+
+        for (let i = 0; i < marks.length; i++) {
+          arrayMarksNumber[i] = Number.parseInt(marks[i]);
+          sum += arrayMarksNumber[i];
+        }
+
+        roundedAverageMark = Math.round(sum / arrayMarksNumber.length);
+
+    }    
+
+    return roundedAverageMark;
+
 }
 
+
+// task 3
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    
+    let now = Date.now(new Date());
+    let arrayBirthday = birthday.split('-');    
+    let birthdayDate = +new Date(arrayBirthday[0], arrayBirthday[1] - 1, arrayBirthday[2]);
+    let age = (now - birthdayDate) / (1000 * 60 * 60 * 24 * 365.25);
+
+    return age >= 18 ? true : false;
+    
 }
